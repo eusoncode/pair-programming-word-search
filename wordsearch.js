94aa1169b24ed69b2518c5942d28ff3e40fc571e
eusoncode <git@github.com:eusoncode/Kata.git>
2023-07-16 14:30:03 -0600

    Horizontal and vertical word search

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,41 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const wordSearch = (letters, word) => {
+  // For Horizontal word search
+  const horizontalJoin = letters.map(ls => ls.join(''));
 
-module.exports = wordSearch
+  for (let l of horizontalJoin) {
+
+    if (l.includes(word)) {
+      return true;
+    }
+  }
+
+  // For Horizontal word search
+  const transposedLetters = transpose(letters);
+  const verticalJoin = transposedLetters.map(ls => ls.join(''));
+
+  for (let l of verticalJoin) {
+    if (l.includes(word)) {
+      return true;
+    }
+  }
+
+  return false;
+};
+
+//Transpose the array to be used in vertical word search
+const transpose = function(matrix) {
+  const newMatrix = [];
+
+  for (let i = 0; i < matrix[0].length; i++) {
+    newMatrix.push([]);
+    for (let j = 0; j < matrix.length; j++) {
+      newMatrix[i][j] = matrix[j][i];
+    }
+  }
+
+  return newMatrix;
+};
+
+module.exports = wordSearch;
+
+
